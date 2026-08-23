10ff4f2d5a3d9fe6e14991db119565cc53672a5f
vanshdhameliya <133960391+vanshdhameliya@users.noreply.github.com>
2026-08-23 18:14:25 +0530

    Time: 6 ms (97.08%), Space: 47.2 MB (10.95%) - LeetHub

--- 1927-sum-game/1927-sum-game.java
+++ 1927-sum-game/1927-sum-game.java
@@ -1,26 +1,42 @@
 class Solution {
     public boolean sumGame(String num) {
         int n = num.length();
+        int half = n / 2;
 
-        int leftSum = 0, rightSum = 0;
-        int leftQ = 0, rightQ = 0;
+        int leftSum = 0;
+        int rightSum = 0;
+        int leftQ = 0;
+        int rightQ = 0;
 
-        for(int i = 0; i < n / 2; i++) {
-            if(num.charAt(i) == '?') {
+        for (int i = 0; i < half; i++) {
+            char c = num.charAt(i);
+
+            if (c == '?') {
                 leftQ++;
             } else {
-                leftSum += num.charAt(i) - '0';
+                leftSum += c - '0';
             }
         }
 
-        for (int i = n / 2; i < n; i++) {
-            if (num.charAt(i) == '?')
+        for (int i = half; i < n; i++) {
+            char c = num.charAt(i);
+
+            if (c == '?') {
                 rightQ++;
-            else {
-                rightSum += num.charAt(i) - '0';
+            } else {
+                rightSum += c - '0';
             }
         }
 
-        return (leftSum - rightSum) * 2 != (rightQ - leftQ) * 9;
+        int qDiff = leftQ - rightQ;
+        int sumDiff = leftSum - rightSum;
+
+        // Odd difference in number of '?' -> Alice wins
+        if (qDiff % 2 != 0) {
+            return true;
+        }
+
+        // Bob can force equality only in this case
+        return sumDiff + 9 * (qDiff / 2) != 0;
     }
 }
